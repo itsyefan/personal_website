b5d69ae8d41fca28c5dc071ff95fb38991d5baf9
itsyefan <ethanyipnz@gmail.com>
2022-05-21 03:17:49 +1200

    fixed full stop

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
         <div class="py-10 px-10 h-128 w-128 2xl:h-156 2xl:w-156 bg-white shadow-2xl rounded-xl relative">
           <h1 class="text-center font-thin text-4xl pb-10 2xl:text-6xl">About Me</h1>
           <p class="text-sm 2xl:text-lg">
-          Hi! My name is Ethan, and I am a penultimate year software engineering student looking for places where I can show off my capabilities.<br/> Some things I can bring to the table include being an observant team player, a clear communicator, and a hard worker .
+          Hi! My name is Ethan, and I am a penultimate year software engineering student looking for places where I can show off my capabilities.<br/> Some things I can bring to the table include being an observant team player, a clear communicator, and a hard worker.
           I believe my previous work experience and leadership positions provide a good reflection of this. <br/><br/>I love taking on the different challenges that are thrown my way and enjoy anything that gets my brain going. I find that getting hands on with things is my
           favourite way of learning (even this website was my way of getting to know the basics of JS, Tailwind and responsive web development). <br/><br/>Outside of software development and academics, I like to play video games and indulge in outdoor activities.
           </p>
